c83bf94783e7d23df60807f2327fe6815350160a
Sakshat Bagadi <bagadisakshat@gmail.com>
2020-09-30 10:40:06 +0530

    Validation of First Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,10 +1,33 @@
 package userRegistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-	
-	public static void main(String[] args) {
-		
-		System.out.println("Welcome to User Registration Validation");
+			
+		public static void validateFirstName(String firstName)
+		{
+			String regex = "^[A-Z]{1}[a-zA-z]{2,}";
+			Pattern pattern = Pattern.compile(regex);
+			Matcher matcher = pattern.matcher(firstName);
+
+			if(matcher.find()) {
+				System.out.println(firstName + " is valid");
+			}
+	                else {
+				System.out.println(firstName + " is not valid");
+	                }
+		}
+		public static void main(String[] args) {
+			
+			System.out.println("Welcome to User Registration Validation");
+			String firstName = "";
+			Scanner input = new Scanner(System.in);
+			System.out.println("Enter the first name");
+			firstName = input.nextLine();
+			validateFirstName(firstName);	
+		}
 	}
 
-}
+
