2af74a8f437d1158054a44df57e18f7df52accba
Sakshat Bagadi <bagadisakshat@gmail.com>
2020-09-30 11:51:58 +0530

    Vaildation of Email

--- UserRegistration.java
+++ UserRegistration.java
@@ -19,21 +19,42 @@ public class UserRegistration {
 				System.out.println(firstName + " is not valid");
 	                }
 		}
+		
+		public static void validateEmail(String Email) {
+			String expression = "(abc[.][A-Za-z]+@bl[.]co[.][A-Za-z]{2,})$"; // Pattern for Email
+			Pattern pattern = Pattern.compile(expression);
+			Matcher match = pattern.matcher(Email);
+			if(match.find()) {
+				System.out.println("Email is Valid");
+			}
+			else {
+			System.out.println("Email is invalid");
+			}
+			
+		}	
+			
+			
 		public static void main(String[] args) {
 			
 			String firstName = "";
 			String lastName = "";
+			String email = "";
 			
 			Scanner input = new Scanner(System.in);
 			
 			System.out.println("Enter the first name");
 			firstName = input.nextLine();
-			validateName(firstName);//validation of first name	
+			validateName(firstName);//validation of first name
 			
 			System.out.println("Enter the last name");
 			lastName = input.nextLine();
-			validateName(lastName);//validation of last name	
+			validateName(lastName);//validation of last name
+			
+			System.out.println("Enter the Email");
+			email = input.nextLine();
+			validateEmail(email);//validation of Email
+		}	
 		}
-	}
+	
 
 
