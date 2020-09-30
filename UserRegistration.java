df80e4dcbb4503d5bfbe1722063e65e81a958548
Sakshat Bagadi <bagadisakshat@gmail.com>
2020-09-30 16:09:52 +0530

    min 8 character in password validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -44,6 +44,20 @@ public class UserRegistration {
 			else
 				System.out.println(mobileNumber + " is not valid");
 		}
+		
+		public static void validatePassword(String password)
+	    {
+	        String regex="[a-zA-Z0-9@_-#$%^&*]{8,}";
+	        Pattern pattern=Pattern.compile(regex);
+	        Matcher matcher=pattern.matcher(password);
+
+	        if(matcher.find()) {
+	            System.out.println(password+" is valid");
+	        }
+	        else {
+	            System.out.println(password+" is not valid");
+	        }
+	    }
 			
 			
 		public static void main(String[] args) {
@@ -70,6 +84,10 @@ public class UserRegistration {
 			System.out.println("Enter the Mobile Number");
 			mobileNumber = input.nextLine();
 			validateMobile(mobileNumber);//validation of MobileNumber
+			
+			System.out.println("Enter the password");
+			String password = input.nextLine();
+			validatePassword(password); // min 8 characters in password
 		}	
 		}
 	
