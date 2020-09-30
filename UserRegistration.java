1e76bda15669305c2b51eb72448bee05c103250c
Sakshat Bagadi <bagadisakshat@gmail.com>
2020-09-30 10:29:06 +0530

    Welcome to Validation of User Registration

---
+++ UserRegistration.java
@@ -0,0 +1,10 @@
+package userRegistration;
+
+public class UserRegistration {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to User Registration Validation");
+	}
+
+}
